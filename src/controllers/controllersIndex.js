4231f60bddb78fef07e045d13f201ef61b2c1247
Fer <fernafleck@gmail.com>
2022-02-12 17:43:40 -0300

    Creacion de /partials de index.ejs

--- src/controllers/controllersIndex.js
+++ src/controllers/controllersIndex.js
@@ -1,7 +1,13 @@
 const path = require("path");
+const fs = require("fs");
+
+//Obtención de Datos del archivo Json
+let productsIndex = JSON.parse(fs.readFileSync(path.resolve(__dirname, "..", "data", "productsIndex.json")));
+
+console.log({productsIndex});
 
 module.exports = {
     index: function (req, res) {
-        res.render(path.resolve(__dirname, "..", "views", "index.ejs"));
+        res.render(path.resolve(__dirname, "..", "views", "index.ejs"), {productsIndex});
     }
 }
